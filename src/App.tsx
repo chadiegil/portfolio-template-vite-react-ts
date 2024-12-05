aa7db5bdc11e877cbeab688ffea784c926d1fa4b
Chadie Gil <caugis@gmail.com>
2024-12-05 09:45:22 +0800

    boiler plate

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Button from "@components/ui/button/button"
+import "./App.css"
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-blue-100">Hello</h1>
+      <Button />
+    </>
+  )
+}
+
+export default App
