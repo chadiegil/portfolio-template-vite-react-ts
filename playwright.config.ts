30cf5b68db522e7f5b1a8e47f88cc40895e92b27
Chadie Gil <caugis@gmail.com>
2024-12-05 15:28:48 +0800

    error test

--- playwright.config.ts
+++ playwright.config.ts
@@ -44,19 +44,19 @@ export default defineConfig({
       use: { ...devices["Desktop Firefox"] },
     },
 
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
+    // {
+    //   name: "webkit",
+    //   use: { ...devices["Desktop Safari"] },
+    // },
 
     {
       name: "Mobile Chrome",
       use: { ...devices["Pixel 5"] },
     },
-    {
-      name: "Mobile Safari",
-      use: { ...devices["iPhone 12"] },
-    },
+    // {
+    //   name: "Mobile Safari",
+    //   use: { ...devices["iPhone 12"] },
+    // },
 
     {
       name: "Microsoft Edge",
